599eb89e9b7f117b4155ab626239e78eaeb3712f
Ouedraogo Sonia De Laeticia <Sonia_De_Laeticia.Ouedraogo@bordeaux-inp.fr>
2019-11-25 14:56:22 +0100

    code module esc

---
+++ src/module_esc.ino
@@ -0,0 +1,21 @@
+#include <Servo.h>
+
+void calibrateEsc(Servo esc){             //at beginning esc must not be powered
+  esc.write(180);                      //sending max throttle to esc
+  if(Serial.available())               //time to power esc then press any key to continue
+    esc.write(0);                      //sending min throttle to esc
+}
+
+
+//sending value v between 0 and 180 corresponding to a certain speed value
+void modifySpeed(Servo esc, int v){
+  // esc.write(0); //sometimes esc needs this step
+  esc.write(v);
+}
+
+
+//updating wheels direction to angle d
+//d  is value in [0,180], 90 for straight line, parallel to wheels
+void modifyDirection(Servo servoMot, int d){
+   servoMot.write(d);
+}
