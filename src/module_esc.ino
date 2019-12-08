8d43c8ed6bf0d162a5bb9d6aaa99c860f1c3387a
Ouedraogo Sonia De Laeticia <Sonia_De_Laeticia.Ouedraogo@bordeaux-inp.fr>
2019-12-08 16:36:57 +0100

    setting esc ans servoMot as global variables

--- src/module_esc.ino
+++ src/module_esc.ino
@@ -1,6 +1,9 @@
 #include <Servo.h>
 
-void calibrateEsc(Servo esc){             //at beginning esc must not be powered
+Servo esc;          
+Servo servoMot;     
+
+void calibrateEsc(){             //at beginning esc must not be powered
   esc.write(180);                      //sending max throttle to esc
   if(Serial.available())               //time to power esc then press any key to continue
     esc.write(0);                      //sending min throttle to esc
@@ -8,7 +11,7 @@ void calibrateEsc(Servo esc){             //at beginning esc must not be powered
 
 
 //sending value v between 0 and 180 corresponding to a certain speed value
-void modifySpeed(Servo esc, int v){
+void modifySpeed(int v){
   // esc.write(0); //sometimes esc needs this step
   esc.write(v);
 }
@@ -16,6 +19,6 @@ void modifySpeed(Servo esc, int v){
 
 //updating wheels direction to angle d
 //d  is value in [0,180], 90 for straight line, parallel to wheels
-void modifyDirection(Servo servoMot, int d){
+void modifyDirection( int d){
    servoMot.write(d);
 }
